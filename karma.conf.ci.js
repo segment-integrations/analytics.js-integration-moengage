920ad3f8b1974a83184b8cc9e386375906fdea2d
Han Kim <han@segment.com>
2017-08-08 18:46:15 -0700

    remove ie support

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -33,21 +33,6 @@ var customLaunchers = {
     browserName: 'safari',
     version: '9.0'
   },
-  sl_ie_9: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '9'
-  },
-  sl_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '10'
-  },
-  sl_ie_11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '11'
-  },
   sl_edge_latest: {
     base: 'SauceLabs',
     browserName: 'microsoftedge'
